e0620215dc3b531415c5b7828a37cba9f2b0f2d2
weihuang023 <weih81888@gmail.com>
2016-02-03 16:40:56 -0500

    Initial Commit

---
+++ src/InterfaceVsAbstractDemo.java
@@ -0,0 +1,108 @@
+public class InterfaceVsAbstractDemo{
+    public static void main(String args[]){
+        Dog dog = new Dog("Max",16);
+        Cat cat = new Cat("Lucy",25);
+
+        System.out.println("Dog:"+dog);
+        System.out.println("Cat:"+cat);
+
+        dog.remember();
+        dog.protectOwner();
+        Learn dl = dog;
+        dl.learn();
+
+
+        cat.remember();
+        cat.protectOwner();
+
+        Climb c = cat;
+        c.climb();
+
+        Man man = new Man();
+        Climb cm = man;
+        cm.climb();
+        Think t = man;
+        t.think();
+        Learn l = man;
+        l.learn();
+        Apply a = man;
+        a.apply();
+
+    }
+}
+
+abstract class Animal{
+	
+    String name;
+    int lifeExpentency;
+    
+    public Animal(String name,int lifeExpentency ){
+        this.name = name;
+        this.lifeExpentency=lifeExpentency;
+    }
+    
+    public abstract void remember();
+    public abstract void protectOwner();
+
+    public String toString(){
+        return this.getClass().getSimpleName()+":"+name+":"+lifeExpentency;
+    }
+    
+}
+class Dog extends Animal implements Learn{
+
+    public Dog(String name,int age){
+        super(name,age);
+    }
+    public void remember(){
+        System.out.println(this.getClass().getSimpleName()+" can remember for 5 minutes");
+    }
+    public void protectOwner(){
+        System.out.println(this.getClass().getSimpleName()+ " will protect owner");
+    }
+    public void learn(){
+        System.out.println(this.getClass().getSimpleName()+ " can learn:");
+    }
+}
+class Cat extends Animal implements Climb {
+    public Cat(String name,int age){
+        super(name,age);
+    }
+    public void remember(){
+        System.out.println(this.getClass().getSimpleName() + " can remember for 16 hours");
+    }
+    public void protectOwner(){
+        System.out.println(this.getClass().getSimpleName()+ " won't protect owner");
+    }
+    public void climb(){
+        System.out.println(this.getClass().getSimpleName()+ " can climb");
+    }
+}
+interface Climb{
+    public void climb();
+}
+interface Think {
+    public void think();
+}
+
+interface Learn {
+    public void learn();
+}
+interface Apply{
+    public void apply();
+}
+
+class Man implements Think,Learn,Apply,Climb{
+    public void think(){
+        System.out.println("I can think:"+this.getClass().getSimpleName());
+    }
+    public void learn(){
+        System.out.println("I can learn:"+this.getClass().getSimpleName());
+    }
+    public void apply(){
+        System.out.println("I can apply:"+this.getClass().getSimpleName());
+    }
+    public void climb(){
+        System.out.println("I can climb:"+this.getClass().getSimpleName());
+    }
+}
